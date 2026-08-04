7736e434267dbb893d5379515a8bdbf7e5ad22e1
HyeonTae Jeon <wkghd951223@gmail.com>
2026-08-04 15:56:59 +0900

    feat Ads.txt

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,12 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
+        source: "/ads.txt",
+        destination:
+          "https://adstxt.journeymv.com/sites/5b2560fd-53fe-4920-872c-67302bb9311c/ads.txt",
+        permanent: true,
+      },
+      {
         source: "/game",
         destination: "/practice?mode=note",
         permanent: false,
